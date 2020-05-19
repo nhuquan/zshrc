b25d2de445dff1e47f50c404209d6a9524f3b2a8
nqdang <nqdang@gael.fr>
2020-05-19 08:13:17 +0200

    update install

--- install.sh
+++ install.sh
@@ -9,6 +9,17 @@ sudo apt install -y \
   gnome-shell-extension-dashtodock \
   gnome-tweak-tool
 
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+cat .zshrc > $HOME/.zshrc
+
+# Install bat
+wget https://github.com/sharkdp/bat/releases/download/v0.15.1/bat_0.15.1_amd64.deb
+
+sudo dpkg -i bat*.deb
+rm bat*.deb
+
 # Install Vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
